580dfdfb25c2fab3b67fc9f08fdf540e1f8ce7be
Reece Crone <reece.crone@gmail.com>
2026-04-14 14:41:20 -0600

    goals now have periods for which they are intented, types that describe their nature and complete by dates

--- mile_stone/goals/models.py
+++ mile_stone/goals/models.py
@@ -2,23 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Goal(models.Model):
-    DAILY = 'daily'
-    WEEKLY = 'weekly'
-    MONTHLY = 'monthly'
-    LONG_TERM = 'long'
+    DAY = 'day'
+    WEEK = 'weekly'
+    MONTH = 'monthly'
+    LONG = 'long'
 
-    GOAL_TYPES = [
-        (DAILY, 'Daily'),
-        (WEEKLY, 'Weekly'),
-        (MONTHLY, 'Monthly'),
-        (LONG_TERM, 'Long Term'),
+    FITNESS = 'fitness'
+    ACADEMIC = 'academic'
+    CAREER = 'career'
+    LIFE = 'life'
+
+    # denotes if a goal is to be completed that day, week, month, or over a longer period
+    GOAL_PERIOD = [
+        (DAY, 'Day'),
+        (WEEK, 'Week'),
+        (MONTH, 'Month'),
+        (LONG, 'Long Term'),
+    ]
+
+    # describes the nature of the goal
+    GOAL_TYPE = [
+        (FITNESS, 'Fitness'),
+        (ACADEMIC, 'Academic'),
+        (CAREER, 'Career'),
+        (LIFE, 'Life'),
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    goal_type = models.CharField(max_length=10, choices=GOAL_TYPES)
-
-    target_value = models.IntegerField(default=1)  # e.g. 5 workouts/week
+    goal_type = models.CharField(max_length=10, choices=GOAL_TYPE)
+    goal_period = models.CharField(max_length=10, choices=GOAL_PERIOD)
+    target_value = models.IntegerField(default=1)  # e.g. 20 miles in a week
+    complete_by_date = models.DateField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
