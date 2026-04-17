781879cb5116b1d7c946cdaa6be8223b370d67a4
Reece <reece.crone@gmail.com>
2026-04-17 12:56:35 -0600

    Implemented a day goals view that lets you observe the goals set for the day period

--- mile_stone/goals/urls.py
+++ mile_stone/goals/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import main,  dayGoals
 
 urlpatterns = [
     path('', main, name='main'),
+    path('daygoals/', dayGoals, name='daygoals')
 ]
--- mile_stone/goals/views.py
+++ mile_stone/goals/views.py
@@ -6,3 +6,12 @@ from .models import Goal
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+def dayGoals(request):
+    goals = Goal.objects.filter(goal_period='day')
+
+    context = {
+        "dailygoals": goals
+    }
+
+    return render(request, 'daygoals.html', context)
