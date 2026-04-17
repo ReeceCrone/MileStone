34f32b828832b6319aa043b484290b12d211a8b2
Reece <reece.crone@gmail.com>
2026-04-17 13:51:26 -0600

    added djangos built in authentication and added authentication protection to the current views

--- mile_stone/goals/views.py
+++ mile_stone/goals/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 from .models import Goal
 
+@login_required
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
 
+@login_required
 def dayGoals(request):
-    goals = Goal.objects.filter(goal_period='day')
+    goals = Goal.objects.filter(user=request.user)
 
     context = {
-        "dailygoals": goals
+        "dailygoals": goals.filter(goal_period='day')
     }
 
     return render(request, 'daygoals.html', context)
